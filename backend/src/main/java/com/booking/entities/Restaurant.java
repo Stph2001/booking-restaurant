a23a82335813f663d1789c8dd6525a1d5c72bb86
Stephano <stephanoml@hotmail.com>
2021-04-06 12:17:52 -0500

    Mapeo de la clase restaurante

---
+++ backend/src/main/java/com/booking/entities/Restaurant.java
@@ -0,0 +1,23 @@
+package com.booking.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "restaurants")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String image;
+}
